fedab594be757013393f20f5f7220d816c8b02aa
Björn Wiberg <crx@qlik.com>
2021-02-01 17:34:54 +0100

    style: meal card

--- src/components/MealCard/index.js
+++ src/components/MealCard/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './MealCard.scss';
+
 const MealCard = ({ id, name, thumbnail }) => (
   <div className="meal-card" data-testid="meal-card" key={name}>
     <NavLink to={`/meal/${id}`} data-testid="meal-card-link">
-      <div>{name}</div>
       <div className="meal-card--thumbnail">
-        <img alt={name} src={thumbnail} data-testid="meal-card-thumbnail" />
+        <div className="meal-card--thumbnail-wrapper">
+          <img alt={name} src={thumbnail} data-testid="meal-card-thumbnail" />
+        </div>
       </div>
+      <div className="meal-card--name">{name}</div>
     </NavLink>
   </div>
 );
