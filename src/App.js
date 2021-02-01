b0226abee8af7cde6bf58736a87d49cf1dd7ee11
Björn Wiberg <crx@qlik.com>
2021-02-01 23:19:11 +0100

    fix: warnings for class not className

--- src/App.js
+++ src/App.js
@@ -22,17 +22,18 @@ export default function App() {
             <div className="wrapper">
               <SiteName text="Meal App" />
               <div className="navigation-wrapper">
+                <div className="favorites">♥ ({favorites.length})</div>
                 <div className="desktop-navigation">
                   <Navigation />
                 </div>
                 <div className="mobile-app-bar-buttons">
                   <nav
-                    class={`mobile-navigation-toggler ${mobileNavigationOpen ? 'active' : ''}`}
+                    className={`mobile-navigation-toggler ${mobileNavigationOpen ? 'active' : ''}`}
                     onClick={() => setMobileNavigationOpen((p) => !p)}
                   >
-                    <div class="bar1"></div>
-                    <div class="bar2"></div>
-                    <div class="bar3"></div>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
                   </nav>
                 </div>
               </div>
